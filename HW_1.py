74c4fc860d71143ec66e411f4a65f11569a24956
Meiram <133873249+Salam78@users.noreply.github.com>
2023-07-23 17:36:30 +0600

    Delete HW_1.py

--- HW_1.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Введите трехзначное число: "))
-
-digit_1 = number // 100
-digit_2 = (number // 10) % 10
-digit_3 = number % 10
-
-sum_of_digits = digit_1 + digit_2 + digit_3
-
-print("Сумма цифр трехзначного числа:", sum_of_digits)
